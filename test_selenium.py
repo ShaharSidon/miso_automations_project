dc5837cfe49025fde2007fb0510aae4cda49c123
Daniel Galin <Danielgalin03@gmail.com>
2023-03-06 15:02:25 +0200

    message

--- test_selenium.py
+++ test_selenium.py
@@ -37,3 +37,29 @@ class TestOrangeHrmLoginPage(unittest.TestCase):
         login_button.click()
         assert self.chrome_driver.current_url == 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/' \
                                                  'index'
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set up the Chrome driver and navigate to the login page
+driver = webdriver.Chrome()
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+# Enter login credentials and submit the form
+username_field = driver.find_element(By.ID, "txtUsername")
+password_field = driver.find_element(By.ID, "txtPassword")
+submit_button = driver.find_element(By.ID, "btnLogin")
+username_field.send_keys("Admin")
+password_field.send_keys("admin123")
+submit_button.click()
+
+# Wait for the dashboard to load and verify that the page title contains "Dashboard"
+dashboard_title = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), 'Dashboard')]"))
+)
+assert "Dashboard" in driver.title
+
+# Close the browser window
+driver.quit()
