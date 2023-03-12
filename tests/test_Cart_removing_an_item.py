fdb83b545066aa4736f7807d5bffe6eda40c11b7
Shahar Sidon <shaharsidon2+git@gmail.com>
2023-03-12 13:21:18 +0200

    adding notes

--- tests/test_Cart_removing_an_item.py
+++ tests/test_Cart_removing_an_item.py
@@ -19,26 +19,33 @@ class RemovingAnItem(unittest.TestCase):
     def tearDown(self) -> None:
         self.driver.quit()
 
+    # the start of the test
     def test_1_removing_an_item_from_the_cart(self):
+        # finding and pressing on a product
         first_button = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH,
                                                                                            '/html/body/div[5]/div/'
                                                                                            'div[2]/div/div[1]/div/'
                                                                                            'div/h4')))
         first_button.click()
+        # finding and pressing the "add to cart" button
         second_button = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH,
                                                                                             '/html/body/div['
                                                                                             '5]/div/div[2]/div['
                                                                                             '2]/div/a')))
         second_button.click()
+        # finding and pressing the cart button at the navigation bar
         third_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#cartur"
                                                                                             )))
         third_button.click()
+        # asserting that the correct page was opened
         assert self.driver.current_url == "https://www.demoblaze.com/cart.html"
+        # finding and pressing on the button to remove an item from the cart
         fourth_button = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#tbodyid > "
                                                                                                          "tr > "
                                                                                                          "td:nth"
                                                                                                          "-child(4) > "
                                                                                                          "a")))
         fourth_button.click()
+        # asserting that a change has happened
         assert self.driver.current_url == "https://www.demoblaze.com/cart.html#"
 
