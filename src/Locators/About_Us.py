7a690d94f01b0a74122fee10f25b4bcf866d1cc7
DanielG1968 <danielgalin03@gmail.com>
2023-03-15 14:06:03 +0200

    Amen

---
+++ src/Locators/About_Us.py
@@ -0,0 +1,6 @@
+class AboutUs:
+    # find about us locators
+    about_us_title = 'videoModalLabel'
+    x_button_xpath = '/html/body/div[4]/div/div/div[1]/button/span'
+    play_button_xpath = '/html/body/div[4]/div/div/div[2]/form/div/div/button/span[2]'
+    close_xpath = '/html/body/div[4]/div/div/div[3]/button'
---
+++ src/Locators/Place_Order.py
@@ -0,0 +1,12 @@
+class PlaceOrderLocators:
+    place_order_txt = '/html/body/div[3]/div/div/div[1]/h5'
+    total_txt = 'totalm'
+    name_field_id = 'name'
+    country_field_id = 'country'
+    city_field_id = 'city'
+    credit_card_field_id = 'card'
+    month_field_id = 'month'
+    year_field_id = 'year'
+    purchase_btn_xpath = '/html/body/div[3]/div/div/div[3]/button[2]'
+    close_btn_xpath = '/html/body/div[3]/div/div/div[3]/button[1]'
+    x_button_xptah = '/html/body/div[3]/div/div/div[1]/button/span'
