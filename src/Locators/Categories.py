09ebb27714f646c1ec4c9ff56516ed3b39b1db9c
Shahar Sidon <shaharsidon2+git@gmail.com>
2023-03-15 13:29:30 +0200

    adding locators

---
+++ src/Locators/Categories.py
@@ -0,0 +1,12 @@
+class CategoriesLocators:
+    """
+    the locations of the elements in the categories menu
+    """
+    categories_title_xpath = '/html/body/div[5]/div/div[1]/div/a[1]'
+
+    phones_button_xpath = '/html/body/div[5]/div/div[1]/div/a[2]'
+
+    laptops_button_xpath = '/html/body/div[5]/div/div[1]/div/a[3]'
+
+    monitors_button_xpath = '/html/body/div[5]/div/div[1]/div/a[4]'
+
---
+++ src/Locators/Product_Information.py
@@ -0,0 +1,11 @@
+class ProductLocators:
+    """
+    the locations of the elements in the categories menu
+    """
+    image_selector = '#imgp > div > img'
+    title_selector = '#tbodyid > h2'
+    price_selector = '#tbodyid > h3'
+    description_title_selector = '#more-information > strong'
+    description_text_selector = '#more-information > p'
+    add_to_cart_button_selector = '#tbodyid > div.row > div > a'
+3
